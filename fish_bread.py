7f3ab38ab5cf24d2be38ad4461b8b8f51accc6f3
eunjaegu <kuhj1021@gmail.com>
2025-03-17 17:32:42 +0900

    ✨ 개발 브랜치 생성
    모드를 입력받을 수 있는 기능 구현

--- fish_bread.py
+++ fish_bread.py
@@ -6,3 +6,25 @@
 #
 #
 #
+
+def order_bread():
+    print("주문 완료")
+
+def admin_mode():
+    print("관리자 모드")
+
+
+while True:
+    mode = input("원하는 모드를 선택하세요 (주문, 관리자, 종료) : ")
+    if mode == "종료":
+        break
+
+    elif mode == "주문":
+        order_bread()
+
+    elif mode == "관리자":
+        admin_mode()
+
+    elif mode == "데이터 분석":
+        pass
+
