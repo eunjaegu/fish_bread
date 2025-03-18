3da1ba6d7dddde8bb356d80fa3d4cd7ddd158339
eunjaegu <kuhj1021@gmail.com>
2025-03-18 17:37:39 +0900

    ✨ 총 매출 계산기 기능 추가
    붕어빵 판매 개수와 붕어빵 가격이 포함된 메뉴판 기능 생성

--- fish_bread.py
+++ fish_bread.py
@@ -1,17 +1,29 @@
-#
-#
-#
-#
-#
-#
-#
-#
+# 붕어빵 가격
+stock = {
+    "팥붕어빵" : 10,      #10개
+    "슈크림붕어빵" : 5,   # 5개
+    "초코붕어빵" : 6     # 6개
+}
 
-def order_bread():
-    print("주문 완료")
+sales = {
+    "팥붕어빵" : 0,      #10개
+    "슈크림붕어빵" : 0,   # 5개
+    "초코붕어빵" : 0     # 6개
+}
 
-def admin_mode():
-    print("관리자 모드")
+prices = {
+    "팥붕어빵" : 1000,      #10개
+    "슈크림붕어빵" : 1200,   # 5개
+    "초코붕어빵" : 1500     # 6개
+}
+
+
+def calculate_salse():
+    total_sales = 0
+
+    for bread in sales:
+        total_sales += (sales[bread] * prices[bread])
+    print(f"총 매출은 {total_sales}원입니다.")
 
 
 while True:
@@ -23,8 +35,14 @@ while True:
         order_bread()
 
     elif mode == "관리자":
-        admin_mode()
+        admin_bread()
 
     elif mode == "데이터 분석":
         pass
+
+calculate_salse()
+
+
+
+
 
